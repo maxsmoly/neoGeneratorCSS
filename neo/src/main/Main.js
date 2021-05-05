148efd5deabed025a094b4dcb1a7832eb5524f92
Максим Смоляков <maksimsmolyakov@MacBook-Air-Maksim.local>
2021-05-05 22:03:18 +0300

    shadow string function

--- neo/src/main/Main.js
+++ neo/src/main/Main.js
@@ -6,11 +6,21 @@ class Main extends React.Component {
 		super(props)
 		this.state = {
 			count: 10,
-			resultWidth: 100,
+			resultWidth: 190,
 			resultRadius: 20,
+			bgResult: '#e0e0e0',
+			horizontal: 22,
+			vertical: 22,
+			blur: 60,
+			colorOne: '#bebebe',
+			negativeHorizont: -22,
+			negatiVertical: -22,
+			colorTwo: '#FFFFFF',
+			shadowRes: '',
 		}
 		this.rangeSize = this.rangeSize.bind(this)
 		this.rangeRadius = this.rangeRadius.bind(this)
+		this.shadowString = this.shadowString.bind(this)
 	}
 	rangeSize(e) {
 		this.setState({ resultWidth: +e.target.value })
@@ -18,21 +28,48 @@ class Main extends React.Component {
 	rangeRadius(e) {
 		this.setState({ resultRadius: +e.target.value })
 	}
+	shadowString() {
+		let a = this.state.horizontal + 'px '
+		let b = this.state.vertical + 'px '
+		let c = this.state.blur + 'px '
+		let d = ',' + this.state.negativeHorizont + 'px '
+		let e = this.state.negatiVertical + 'px '
+		this.setState({ horizontal: a })
+		this.setState({ vertical: b })
+		this.setState({ blur: c })
+		this.setState({ colorOne: this.state.colorOne })
+		this.setState({ shadowRes: a + b + c + this.state.colorOne + d + e + c + this.state.colorTwo })
+		console.log(this.state.shadowRes)
+	}
 
 	render() {
 		let resultStyle = {
 			width: this.state.resultWidth,
 			height: this.state.resultWidth,
 			borderRadius: this.state.resultRadius,
+			background: this.state.bgResult,
+			boxShadow: this.state.shadowRes,
 		}
+
+		console.log(resultStyle)
+		console.log(this.state.shadowRes)
+
 		return (
 			<main>
-				<div className='main'>
+				<div className='main' style={{ background: this.state.bgResult }}>
 					<div className='render-block'>
 						<div className='result' style={resultStyle}></div>
 					</div>
 					<div className='settings'>
-						<input className='result-size' onChange={this.rangeSize} type='range' min='50' max='200' step='1' />
+						<input
+							className='result-size'
+							onInput={this.rangeSize}
+							type='range'
+							min='50'
+							max='200'
+							step='1'
+							defaultValue={this.state.resultWidth}
+						/>
 						<p>{this.state.resultWidth + 'px'}</p>
 						<input
 							className='result-radius'
@@ -41,9 +78,29 @@ class Main extends React.Component {
 							min='0'
 							max='100'
 							step='1'
-							defaultValue='10'
+							defaultValue={this.state.resultRadius}
 						/>
 						<p>{this.state.resultRadius + 'px'}</p>
+						<input
+							className='result-distance'
+							onChange={this.rangeSize}
+							type='range'
+							min='0'
+							max='100'
+							step='1'
+							defaultValue='10'
+						/>
+						<p></p>
+						<input
+							className='result-blur'
+							onChange={this.rangeRadius}
+							type='range'
+							min='0'
+							max='100'
+							step='1'
+							defaultValue='10'
+						/>
+						<p></p>
 						<div className='code-result'>
 							<p>
 								<pre>
@@ -54,6 +111,9 @@ class Main extends React.Component {
 										<br />
 										border-radius: {this.state.resultRadius + 'px'};
 										<br />
+										background: {this.state.bgResult}
+										<br />
+										box-shadow:{this.state.shadowRes}
 									</code>
 								</pre>
 							</p>
