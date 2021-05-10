fca3be61fd4f09a4b3bf236c9950dd0d3fd76988
maxsmoly <stademanmark@gmail.com>
2021-05-10 19:34:29 +0300

    add colorValue function

--- neo/src/main/Main.js
+++ neo/src/main/Main.js
@@ -39,7 +39,14 @@ class Main extends React.Component {
 		this.setState({ negatiVertical: -+e.target.value })
 	}
 	colorValue(e) {
-		console.log(e.target.value)
+		let hex = e.target.value
+		const hexToRgb = hex
+			.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (m, r, g, b) => '#' + r + r + g + g + b + b)
+			.substring(1)
+			.match(/.{2}/g)
+			.map((x) => parseInt(x, 16))
+
+		console.log(hexToRgb)
 	}
 
 	render() {
